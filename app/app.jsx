3480e987a01173a5a7b259d98802f4ec6fa8a835
wildlifehexagon <crossthebreeze@gmail.com>
2017-06-24 19:30:46 -0500

    support foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,6 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App CSS
